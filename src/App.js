d724299d4a8f1a5328d90747fcf611e9abb91684
mjwils <martin26jw@gmailcom>
2022-08-26 14:33:24 +0100

    agaeagaeg

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Map } from "./Map/Map";
+
+import "leaflet/dist/leaflet.css";
+import "./app.css";
+import "antd/dist/antd.variable.min.css";
+import "leaflet.markercluster/dist/MarkerCluster.css";
+import "leaflet.markercluster/dist/MarkerCluster.Default.css";
+import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
+
+export const App = () => {
+  return <Map />;
+}
